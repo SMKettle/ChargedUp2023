bcf4fcf6ffa98a16b678f18795a6f7a78654ddbf
kirbt <91921906+kirbt@users.noreply.github.com>
2023-10-26 19:10:42 -0700

    commands

---
+++ src/main/java/frc/team2412/robot/commands/test/TestBackwardCommand.java
@@ -0,0 +1,8 @@
+package frc.team2412.robot.commands.test;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team2412.robot.subsystems.TestSubsystem
+
+public class TestBackwardCommand extends CommandBase{
+    
+}
--- src/main/java/frc/team2412/robot/commands/test/TestForwardCommand.java
+++ src/main/java/frc/team2412/robot/commands/test/TestForwardCommand.java
@@ -13,15 +13,33 @@ public class TestForwardCommand extends CommandBase {
     public TestForwardCommand(TestSubsystem testSubsystem) {
         this.testSubsystem = testSubsystem;
 
-
+        // makes it so you cant use multiple commands at once in a subsystem
         addRequirements(testSubsystem);
     }
 
-
-
     // methods
 
+    @Override
+    public void initialize() {
+        // code here
+        testSubsystem.setMotorForward(); 
 
+    }
+
+    @Override
+    public void execute() {
+        // loops set of code on robot
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // runs when command stops
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 
 
 }
